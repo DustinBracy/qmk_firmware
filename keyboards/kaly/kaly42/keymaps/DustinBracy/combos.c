f3fc2b43534d9f70a04107f4c3cd4faa791dc3a2
Dustin Bracy <dustin.bracy@flmarine.com>
2024-09-19 10:08:22 -0500

    add f12 combo, remove tab+escape combos

--- keyboards/kaly/kaly42/keymaps/DustinBracy/combos.c
+++ keyboards/kaly/kaly42/keymaps/DustinBracy/combos.c
@@ -1,16 +1,19 @@
 // added per https://github.com/qmk/qmk_firmware/issues/21137
 
 enum myCombos {
-    ESC_COMBO,
-    TAB_COMBO,
+    // ESC_COMBO,
+    // TAB_COMBO,
+    F12_COMBO,
     COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM esc_combo[] = {KC_W, KC_Q, COMBO_END};
-const uint16_t PROGMEM tab_combo[] = {KC_A, KC_R, COMBO_END};
+// const uint16_t PROGMEM esc_combo[] = {KC_W, KC_Q, COMBO_END};
+// const uint16_t PROGMEM tab_combo[] = {KC_A, KC_R, COMBO_END};
+const uint16_t PROGMEM f12_combo[] = {KC_QUOT, KC_BSLS, COMBO_END};
 
 combo_t key_combos[] = {
-    [ESC_COMBO] = COMBO(esc_combo, KC_ESC),
-    [TAB_COMBO] = COMBO(tab_combo, KC_TAB)
+    // [ESC_COMBO] = COMBO(esc_combo, KC_ESC),
+    // [TAB_COMBO] = COMBO(tab_combo, KC_TAB)
+    [F12_COMBO] = COMBO(f12_combo, KC_F12)
 };
